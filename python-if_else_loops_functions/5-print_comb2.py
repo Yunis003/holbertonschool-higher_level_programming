1e6ddeb85a3e31427080d32e5df52a54c6edb9d0
Yunis003 <hi.yunisqulamov@gmail.com>
2024-09-07 16:35:54 +0000

    fivth_task2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    print("{:02d}".format(i), end = ", " if i < 99 else "\n")
+for n in range(100):
+    print("{:02d}".format(n), end=", " if n < 99 else "\n")
